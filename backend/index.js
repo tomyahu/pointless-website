d297a0d3142027311387961848a657314624fec6
tomimi <tomyahu@gmail.com>
2023-11-27 19:09:14 -0300

    made form work

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,18 @@ import express from "express";
 import { PORT } from "./config.js"
 import authRoute from "./routes/authRoutes.js"
 import sessionRoute from "./routes/sessionRoutes.js"
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+		methods: [ 'GET', 'POST', 'PUT', 'DELETE' ],
+		allowedHeaders: [ 'Content-Type' ],
+	})
+);
 
 app.use( '/auth', authRoute );
 app.use( '/session', sessionRoute );
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,42 @@
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+
 const LoginForm = () => {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+
+	async function login() {
+		const data = {
+			user: username,
+			pass: password
+		}
+
+		axios.post( "http://localhost:5555/auth", data)
+		.then( (response) => {
+			console.log(response);
+		})
+
+		.catch( (error) => {
+			console.log(error);
+		})
+	}
+
 	return (
-		<form>
+		<div>
 			<div>
-				<label for="username">Username </label>
-				<input name="username" type="text"></input>
+				<label htmlFor="username">Username </label>
+				<input name="username" type="text" onChange={(event) => { setUsername(event.target.value) }} />
 			</div>
 
 			<div>
-				<label for="pass">Password </label>
-				<input name="pass" type="password"></input>
+				<label htmlFor="pass">Password </label>
+				<input name="pass" type="password" onChange={(event) => { setPassword(event.target.value) }} />
 			</div>
 
-			<input type="submit" value="Send"></input>
-		</form>
+			<button type="submit" onClick={login}>Send</button>
+		</div>
 	)
 }
 
